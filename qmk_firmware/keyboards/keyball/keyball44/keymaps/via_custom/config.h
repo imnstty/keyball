258820dc9effca1327243b7446cad69953ca21a7
GTUNEE5\imani <imanishi.tetsuya@gmail.com>
2026-06-28 09:20:33 +0900

    【QMK】単打と押しっ放しの実装：https://oookaworks.seesaa.net/article/465349890.html#gsc.tab=0

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/config.h
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/config.h
@@ -37,4 +37,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define POINTING_DEVICE_AUTO_MOUSE_ENABLE
 #define AUTO_MOUSE_DEFAULT_LAYER 1
+
 #define DYNAMIC_KEYMAP_LAYER_COUNT 6
+
+#define PERMISSIVE_HOLD
+#define RETRO_TAPPING
