4687d7b3fb9fd2a4941e1759953a3d6f21f49ee1
GTUNEE5\imani <imanishi.tetsuya@gmail.com>
2026-07-09 00:58:46 +0900

    KEM Ver.2.22 Step4-S1: Move KEM_L5 target layer to 4 KEM Ver.2.22 Step4-D3: Add hold LED transition by tapping term

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/features/kem_layer.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/features/kem_layer.c
@@ -7,12 +7,15 @@ Copyright 2026 Tetsuya Imanishi
  * @project   Keyball44 Custom Firmware
  * @brief     KEM Layer Engine
  *
- * @version   2.00
- * @date      2026-07-04
+ * @version   2.20
+ * @date      2026-07-08
  *
  *-----------------------------------------------------------------------------
  * Revision History
  *-----------------------------------------------------------------------------
+ * Ver 2.22  2026-07-08
+ * - Changed KEM_L5 target layer from Layer5 to Layer4.
+ *
  * Ver 2.00  2026-07-04
  * - Added Tap/Hold Layer5 key.
  *
@@ -21,52 +24,65 @@ Copyright 2026 Tetsuya Imanishi
 #include "kem_layer.h"
 #include "kem.h"
 
-#define KEM_LAYER5 5
+#define KEM_LAYER5 4
 
-static bool     kem_l5_active      = false;
-static bool     kem_l5_interrupted = false;
-static uint16_t kem_l5_timer       = 0;
+static bool kem_l5_active = false;
+static bool kem_l5_interrupted = false;
+static uint16_t kem_l5_timer = 0;
 
-bool kem_layer_process_record(uint16_t keycode, keyrecord_t *record) {
-    if (kem_l5_active && keycode != KEM_L5 && record->event.pressed) {
+bool kem_layer_process_record(uint16_t keycode, keyrecord_t *record)
+{
+    if (kem_l5_active && keycode != KEM_L5 && record->event.pressed)
+    {
         kem_l5_interrupted = true;
     }
 
-    switch (keycode) {
-        case KEM_L5:
-            if (record->event.pressed) {
-                kem_l5_active      = true;
-                kem_l5_interrupted = false;
-                kem_l5_timer       = timer_read();
+    switch (keycode)
+    {
+    case KEM_L5:
+        if (record->event.pressed)
+        {
+            kem_l5_active = true;
+            kem_l5_interrupted = false;
+            kem_l5_timer = timer_read();
 
-                layer_on(KEM_LAYER5);
-            } else {
-                bool is_tap = timer_elapsed(kem_l5_timer) < TAPPING_TERM && !kem_l5_interrupted;
+            layer_on(KEM_LAYER5);
+        }
+        else
+        {
+            bool is_tap = timer_elapsed(kem_l5_timer) < TAPPING_TERM && !kem_l5_interrupted;
 
-                kem_l5_active = false;
+            kem_l5_active = false;
 
-                if (is_tap) {
-                    layer_move(KEM_LAYER5);
-                } else {
-                    layer_off(KEM_LAYER5);
-                }
+            if (is_tap)
+            {
+                layer_move(KEM_LAYER5);
             }
-            return false;
+            else
+            {
+                layer_off(KEM_LAYER5);
+            }
+        }
+        return false;
     }
 
     return true;
 }
 
-bool kem_layer_is_l5_active(void) {
+bool kem_layer_is_l5_active(void)
+{
     return kem_l5_active;
 }
 
-bool kem_layer_is_l5_hold_candidate(void) {
+bool kem_layer_is_l5_hold_candidate(void)
+{
     return kem_l5_active && !kem_l5_interrupted;
 }
 
-bool kem_layer_is_l5_hold_confirmed(void) {
-    if (!kem_l5_active) {
+bool kem_layer_is_l5_hold_confirmed(void)
+{
+    if (!kem_l5_active)
+    {
         return false;
     }
 
