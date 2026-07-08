5c138b559079f26c5b2561c07475dfcbb68ba219
GTUNEE5\imani <imanishi.tetsuya@gmail.com>
2026-07-08 09:54:17 +0900

    KEM Ver.2.1 Step5.0-5: Improve OLED Debug Page

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/features/oled.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/features/oled.c
@@ -358,8 +358,8 @@ static void render_page3(void)
     char kc[5];
 
     kc[0] = hex[(last_kc >> 12) & 0x0F];
-    kc[1] = hex[(last_kc >> 8) & 0x0F] aa
-        kc[2] = hex[(last_kc >> 4) & 0x0F];
+    kc[1] = hex[(last_kc >> 8) & 0x0F];
+    kc[2] = hex[(last_kc >> 4) & 0x0F];
     kc[3] = hex[last_kc & 0x0F];
     kc[4] = '\0';
 
