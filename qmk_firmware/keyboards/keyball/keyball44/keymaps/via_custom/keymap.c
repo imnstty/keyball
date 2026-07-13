7dba5f44d1deaaba653eb237c1990c139be22977
GTUNEE5\imani <imanishi.tetsuya@gmail.com>
2026-07-13 23:49:08 +0900

    Merge branch 'main' of github.com:imnstty/keyball
    # Conflicts:
    #	qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
@@ -91,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // =========================================================
     [0] = LAYOUT_universal(
         KC_ESC        , KC_Q     , KC_W   , KC_E     , KC_R     , KC_T     ,                                    KC_Y,    KC_U,    KC_I   , KC_O     , KC_P         , KC_DEL      ,
-        LSFT_T(KC_TAB), KC_A     , KC_S   , KC_D     , KC_F     , KC_G     ,                                    KC_H,    KC_J,    KC_K   ,LT(3,KC_L),LT(1,KC_MINS) ,LT(3,KC_JYEN),
+        LSFT_T(KC_TAB), KC_A     , KC_S   , KC_D     , KC_F     , KC_G     ,                                    KC_H,    KC_J,    KC_K   ,LT(3,KC_L),LT(1,KC_MINS) ,LT(3,KC_INT3),
         KC_LCTL       , KC_Z     , KC_X   , KC_C     , KC_V     , KC_B     ,                                    KC_N,    KC_M,    KC_COMM, KC_DOT   , KC_SLSH      , KC_INT1     ,
                              KC_LALT, KC_LGUI ,LT(3,KC_LNG2),LT(2,KC_SPC),LT(1,KC_ENT),          LT(1,KC_INT4),LT(2,KC_SPC), _______ , _______ , KEM_L5
     ),
