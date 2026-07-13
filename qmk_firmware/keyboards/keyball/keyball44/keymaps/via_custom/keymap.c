95ecda7d9843535cb55674cb1cbc6b6be9d6ce79
GTUNEE5\imani <imanishi.tetsuya@gmail.com>
2026-07-13 23:53:25 +0900

    キーマップ修正

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/via_custom/keymap.c
@@ -120,7 +120,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // Layer 3 : Settings
     // =========================================================
     [3] = LAYOUT_universal(
-        KC_ESC        , K_NUM   , KC_SCRL , KC_PSCR , KC_PAUS     , KC_BSPC ,                                  S(KC_SCLN),S(KC_QUOT) , KC_7 , KC_8 , KC_9 , KC_DEL ,
+        KC_ESC        , KC_NUM  , KC_SCRL , KC_PSCR , KC_PAUS     , KC_BSPC ,                                  S(KC_SCLN),S(KC_QUOT) , KC_7 , KC_8 , KC_9 , KC_DEL ,
         LSFT_T(KC_TAB), KC_CAPS , KC_HOME , KC_PGUP ,LCTL(KC_PGUP), KC_EQL  ,                                   KC_MINS  , KC_SLSH   , KC_4 , KC_5 , KC_6 , KC_NO  ,
         KC_LCTL       , KC_NO   , KC_END  , KC_PGDN ,LCTL(KC_PGDN),S(KC_8)  ,                                  S(KC_9)  , KC_0      , KC_1 , KC_2 , KC_3 , KC_NO  ,
                                   KC_LALT , KC_LGUI , _______ , LT(2,KC_SPC), _______ ,           LT(1,KC_ENT),LT(2,KC_SPC), _______ , _______ , KC_PDOT
